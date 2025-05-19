eb44468dbdace42cf69a5453bcfae3dd144415d5
Wagner Santos <wmedsantos@hotmail.com>
2025-05-19 20:01:11 -0300

    Publicação inicial de teste v1.0.003

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@
 module.exports = function(eleventyConfig) {
   //eleventyConfig.addPassthroughCopy("admin/config.yml");
   //eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/images");
   return {
     dir: {
       input: "src",
