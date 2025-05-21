e6e8071880ed9884e793eaf5aa0055e6d712e357
Wagner Santos <wmedsantos@hotmail.com>
2025-05-21 10:59:05 -0300

    Fix admin v1.0.0021

--- .eleventy.js
+++ .eleventy.js
@@ -7,12 +7,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("publicacoes", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/publicacoes/*.md");
   });
-  //eleventyConfig.addPassthroughCopy("admin/config.yml");
-  //eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/CNAME");
+  // evita injetar o script de live-reload nas URLs que começam com /admin
+  eleventyConfig.setBrowserSyncConfig({
+    snippetOptions: {
+      // blacklist é um array de expressões que,
+      // se corresponderem ao request, não recebem o snippet
+      blacklist: ["/admin/**"]
+    }
+  });
   return {
     dir: {
       input: "src",
