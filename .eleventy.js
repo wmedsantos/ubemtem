9c63aa5d6b7c44a46a81e90810a374d414561510
Wagner Medeiros Santos <wmedsantos@hotmail.com>
2026-06-26 19:02:48 -0300

    Inclui pasta biblioteca no build do Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,3 @@
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("eventos", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/eventos/*.md");
@@ -7,20 +6,24 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("publicacoes", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/publicacoes/*.md");
   });
+
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/_redirects");
+
+  // Publica a biblioteca digital criada fora da pasta src.
+  eleventyConfig.addPassthroughCopy("biblioteca");
+
   // evita injetar o script de live-reload nas URLs que começam com /admin
   eleventyConfig.setBrowserSyncConfig({
     snippetOptions: {
-      // blacklist é um array de expressões que,
-      // se corresponderem ao request, não recebem o snippet
       blacklist: ["/admin/**"]
     }
   });
+
   return {
     dir: {
       input: "src",
